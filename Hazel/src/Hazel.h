6aa9347191c6a845724f2125d9d85dd6775d91d6
wang.zhinang <wzhinapple@163.com>
2020-09-25 01:53:27 +0800

    Setup basic Application and Entry Point.

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <stdio.h>
+
+#include "Hazel/Core.h"
+#include "Hazel/Application.h"
+
+//----------------EntryPoint--------------------------------
+#include "Hazel/EntryPoint.h"
+//---------------------------------------------
+
